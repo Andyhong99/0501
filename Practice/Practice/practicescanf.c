ca50e9e070705403fb9920832aaba6ef725517b4
Shinpyo Hong <you@example.com>
2019-05-01 21:21:49 -0400

    Today practice

---
+++ Practice/Practice/practicescanf.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int num1;
+	float num2;
+	char c3;
+
+	printf("Input the 2 numbers and 1 charcter:");
+	scanf("%d %f %c", &num1, &num2, &c3);
+
+	printf("%d\n", num1);
+	printf("%f\n", num2);
+	printf("%c\n", c3);
+
+
+}
